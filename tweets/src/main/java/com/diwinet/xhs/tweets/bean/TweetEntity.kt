730af4da39cc287e868392abb43baeb569337cf1
dbscarlet <493176547@qq.com>
2018-09-18 11:14:11 +0800

    fix

--- tweets/src/main/java/com/diwinet/xhs/tweets/bean/TweetEntity.kt
+++ tweets/src/main/java/com/diwinet/xhs/tweets/bean/TweetEntity.kt
@@ -3,76 +3,94 @@ package com.diwinet.xhs.tweets.bean
 /**
  * Created by Daibing Wang on 2018/9/12.
  */
-data class UserMention(
-    val name: String,
-    val id_str: String,
-    val id: Int,
+
+data class Hashtag(
     val indices: List<Int>,
-    val screen_name: String
+    val text: String
 )
 
 data class Url(
-    val expanded_url: String?,
-    val url: String,
     val indices: List<Int>,
-    val display_url: String?
-)
-
-data class TweetUser(
-    val id: Int,
-    val id_str: String,
-    val name: String,
-    val screen_name: String,
-    val location: String,
-    val description: String,
     val url: String,
-    val entities: Entities,
-    val protected: Boolean,
-    val followers_count: Int,
-    val friends_count: Int,
-    val listed_count: Int,
-    val created_at: String,
-    val favourites_count: Int,
-    val utc_offset: Int,
-    val time_zone: String,
-    val geo_enabled: Boolean,
-    val verified: Boolean,
-    val statuses_count: Int,
-    val lang: String,
-    val contributors_enabled: Boolean,
-    val is_translator: Boolean,
-    val is_translation_enabled: Boolean,
-    val profile_background_color: String,
-    val profile_background_image_url: String,
-    val profile_background_image_url_https: String,
-    val profile_background_tile: Boolean,
-    val profile_image_url: String,
-    val profile_image_url_https: String,
-    val profile_banner_url: String,
-    val profile_link_color: String,
-    val profile_sidebar_border_color: String,
-    val profile_sidebar_fill_color: String,
-    val profile_text_color: String,
-    val profile_use_background_image: Boolean,
-    val has_extended_profile: Boolean,
-    val default_profile: Boolean,
-    val default_profile_image: Boolean,
-    val following: Boolean,
-    val follow_request_sent: Boolean,
-    val notifications: Boolean,
-    val translator_type: String
-)
-
-data class Entities(
-        val url: EntitiesUrl,
-        val description: Description,
-        val hashtags: List<Any>,
-        val user_mentions: List<UserMention>
+    val display_url: String,
+    val expanded_url: String,
+    val unwound: Unwound?
 ) {
-    data class Description(
-        val urls: List<Url>
-    )
-    data class EntitiesUrl(
-        val urls: List<Url>
+
+    data class Unwound(
+        val url: String,
+        val status: Int,
+        val title: String,
+        val description: String
     )
 }
+
+data class UserMention(
+    val name: String,
+    val indices: List<Int>,
+    val screen_name: String,
+    val id_str: String
+)
+
+data class Symbol(
+    val indices: List<Int>,
+    val text: String
+)
+
+data class Poll(
+    val options: List<Option>,
+    val end_datetime: String,
+    val duration_minutes: Int
+) {
+
+    data class Option(
+            val position: Int,
+            val text: String
+    )
+}
+
+data class Media(
+    val type: String,
+    val sizes: Sizes,
+    val indices: List<Int>,
+    val url: String,
+    val media_url: String,
+    val display_url: String,
+    val id_str: String,
+    val expanded_url: String,
+    val media_url_https: String,
+    val source_status_id_str: String?
+) {
+
+    data class Sizes(
+            val thumb: Thumb,
+            val large: Large,
+            val medium: Medium,
+            val small: Small
+    )
+
+    data class Large(
+            val h: Int,
+            val resize: String,
+            val w: Int
+    )
+
+    data class Small(
+            val h: Int,
+            val resize: String,
+            val w: Int
+    )
+
+    data class Medium(
+            val h: Int,
+            val resize: String,
+            val w: Int
+    )
+
+    data class Thumb(
+            val h: Int,
+            val resize: String,
+            val w: Int
+    )
+}
+
