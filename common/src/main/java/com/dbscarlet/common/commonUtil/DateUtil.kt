a116a7c8923be60f72adb79c8fddf56ba06053a7
dbscarlet <493176547@qq.com>
2019-01-10 15:17:50 +0800

    fix

--- common/src/main/java/com/dbscarlet/common/commonUtil/DateUtil.kt
+++ common/src/main/java/com/dbscarlet/common/commonUtil/DateUtil.kt
@@ -6,13 +6,33 @@ import java.util.*
 /**
  * Created by Daibing Wang on 2018/4/27.
  */
-fun Long.toFormateDate(regulation: String) : String {
-    return SimpleDateFormat(regulation)
-            .format(Date(this))
+
+/**
+ * 时间戳转[Date]
+ */
+fun Long.toDate(): Date {
+    return Date(this)
 }
 
-fun String.toTimestamp(regulation: String) : Long {
-    return SimpleDateFormat(regulation)
+/**
+ * 时间戳按格式[format]转日期字符串
+ */
+fun Long.toDateString(format: String): String {
+    return SimpleDateFormat(format)
+            .format(this.toDate())
+}
+
+/**
+ * 日期字符串转[Date]
+ */
+fun String.toDate(format: String): Date {
+    return SimpleDateFormat(format)
             .parse(this)
-            .time
+}
+
+/**
+ * 日期字符串转时间戳
+ */
+fun String.toTimestamp(format: String): Long {
+    return toDate(format).time
 }
